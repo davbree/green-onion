40cd386786406c1109fa57a76c97a375df08c2e6
Stackbit Code Editor <projects@stackbit.com>
2021-10-06 15:01:22 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -33,22 +33,22 @@ module.exports = {
         // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
         // function to update the content on the page without refreshing the
         // whole page
-        config.plugins.push(new webpack.WatchIgnorePlugin({ paths: [/\/content\//] }));
+        // config.plugins.push(new webpack.WatchIgnorePlugin({ paths: [/\/content\//] }));
 
-        if (process.env.ANALYZE === 'true') {
-            const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-            config.plugins.push(
-                new BundleAnalyzerPlugin({
-                    openAnalyzer: true,
-                    analyzerMode: 'server',
-                    analyzerPort: isServer ? 8888 : 8889
-                    // analyzerMode: 'static',
-                    // reportFilename: isServer
-                    //     ? '../analyze/server.html'
-                    //     : './analyze/client.html'
-                })
-            );
-        }
+        // if (process.env.ANALYZE === 'true') {
+        //     const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+        //     config.plugins.push(
+        //         new BundleAnalyzerPlugin({
+        //             openAnalyzer: true,
+        //             analyzerMode: 'server',
+        //             analyzerPort: isServer ? 8888 : 8889
+        //             // analyzerMode: 'static',
+        //             // reportFilename: isServer
+        //             //     ? '../analyze/server.html'
+        //             //     : './analyze/client.html'
+        //         })
+        //     );
+        // }
 
         return config;
     }
